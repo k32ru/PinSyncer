7bae352e55c2c9e6afab26c13a5a880202455e88
k32ru <k32rupc@gmail.com>
2021-05-18 21:47:02 +0900

    BOTが連投を受け付けないので、適当にスリープを追加

--- config_example.py
+++ config_example.py
@@ -4,6 +4,6 @@ api_hash = '31318318349012842390482081188a48'
 
 in_chat_id = -1434343430480  # このチャンネルで sync_start_message での発言を検知すると
 out_chat_id = -1423424230480  # このチャンネルでも /pin updateと発言する
-notic_chat_id = ''  # 更新したことを一緒にお知らせるするチャンネルID
+notice_chat_id = ''  # 更新したことを一緒にお知らせるするチャンネルID
 sync_start_message = 'あ24234は'
 sync_bot_id = 1432232327
